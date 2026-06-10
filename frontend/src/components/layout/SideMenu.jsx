0f6b3ef0b35850fba5c16a63ea68b49b555ada94
sandiththenuja <sandiththenuja2005@gmail.com>
2026-06-10 21:29:06 +0530

    dashboard created

--- frontend/src/components/layout/SideMenu.jsx
+++ frontend/src/components/layout/SideMenu.jsx
@@ -32,18 +32,38 @@ const SideMenu = ({activeMenu}) => {
   }, [user])
 
   return (
-    <div className=''>
-      <div className="">
-        <div className="">
-          <img src={user?.profileImageUrl || ""} alt="Profile Image" className='' />
+    <div className='w-64 h-[calc(100vh-61px)] bg-white border-r broder0gray-200/50 sticky top-15.25 z-20 '>
+      <div className="flex felx-col items-center justify-center mb-7 pt-5">
+        <div className="relative">
+          <img src={user?.profileImageUrl || ""} alt="Profile Image" className='w-20 h-20 bg-slate-400 rounded-full' />
         </div>
 
         {user?.role === "admin" && (
-          <div className="">
+          <div className="text-[10px] font-medium text-white bg-primary px-3 py-0.5 rounded mt-1">
             Admin
           </div>
         )}
+
+        <h5 className="text-gray-950 font-medium leading-6 mt-3">
+          {user?.name || ""}
+        </h5>
+
+        <p className="text-[12px] text-gray-500">{user?.email || ""}</p>
       </div>
+
+      {sideMenuData.map((item, index) => (
+        <button 
+        key={`menu_${index}`}
+        className={`w-full flex items-center gap-4 text-[15px] ${
+          activeMenu == item.label 
+          ? "text-primary bg-linear-to-r from-blue-50/40 to-blue-100/50 border-r-3"
+          : ""
+        } py-3 px-6 mb-3 cursor-pointer`}
+        onClick={() => handleClick(item.path)}>
+          <item.icon className="text-xl" />
+          {item.label}
+        </button>
+      ))}
     </div>
   )
 }
--- frontend/src/pages/user/Dashboard.jsx
+++ frontend/src/pages/user/Dashboard.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
 import { useContext } from 'react'
 import { UserContext } from '../../context/userContext'
 import DashboardLayout from '../../components/layout/DashboardLayout'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
 
 const Dashboard = () => {
   useUserAuth()
 
   const {user} = useContext(UserContext)
+
+  const navigate = useNavigate()
+
+  const [dashboardData, setDashboardData] = useState(null)
+  const [pieChartData, setPieChartData] = useState([])
+  const [barChartData, setBarChartData] = useState([])
+
+  const getDashboardData = async() => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      )
+      if (response.data){
+        setDashboardData(response.data)
+      }
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <DashboardLayout>
-      <div>Dashboard
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+      Dashboard
     </DashboardLayout>
     
   )
