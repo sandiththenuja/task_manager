a2a35e562f9453ff01c8cd79ad21e5e672327e2d
sandiththenuja <sandiththenuja2005@gmail.com>
2026-06-10 20:02:48 +0530

    dasboard created

--- frontend/src/components/layout/SideMenu.jsx
+++ frontend/src/components/layout/SideMenu.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../../context/userContext'
+import {useNavigate} from 'react-router-dom'
+import { SIDE_MENU_DATA, SIDE_MENU_USER_DATA } from '../../utils/data'
 
-const SideMenu = () => {
+const SideMenu = ({activeMenu}) => {
+  const {user, clearUser} = useContext(UserContext)
+  const [sideMenuData, setSideMenuData] = useState([])
+
+  const navigate = useNavigate()
+
+  const handleClick = (route) => {
+    if (route === "logout"){
+      handleLogout()
+      return
+    }
+
+    navigate(route)
+  }
+
+  const handleLogout = () => {
+    localStorage.clear()
+    clearUser()
+    navigate("/login")
+  }
+
+  useEffect(() => {
+    if (user){
+      setSideMenuData(user?.role === "admin" ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA)
+    }
+    return () => {}
+  }, [user])
+
   return (
-    <div>SideMenu</div>
+    <div className=''>
+      <div className="">
+        <div className="">
+          <img src={user?.profileImageUrl || ""} alt="Profile Image" className='' />
+        </div>
+
+        {user?.role === "admin" && (
+          <div className="">
+            Admin
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
