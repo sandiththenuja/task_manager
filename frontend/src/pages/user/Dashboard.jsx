159dff2082a85bf856fc0069ae6d6b0521db0cc1
sandiththenuja <sandiththenuja2005@gmail.com>
2026-06-11 21:05:04 +0530

    dashboard created

--- frontend/src/pages/user/Dashboard.jsx
+++ frontend/src/pages/user/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useUserAuth } from '../../hooks/useUserAuth'
 import { useContext } from 'react'
 import { UserContext } from '../../context/userContext'
@@ -6,6 +6,10 @@ import DashboardLayout from '../../components/layout/DashboardLayout'
 import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPaths'
+import moment from 'moment'
+import InfoCard from '../../components/Cards/InfoCard'
+import { addThousandsSeparator } from '../../utils/helper'
+import {ioMdCard} from 'react-icons/io'
 
 const Dashboard = () => {
   useUserAuth()
@@ -27,13 +31,38 @@ const Dashboard = () => {
         setDashboardData(response.data)
       }
     } catch (error) {
-      
+      console.error("Error fetching users", error);
     }
   }
 
+  useEffect(() => {
+    getDashboardData()
+
+    return () => {}
+  }, [])
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      Dashboard
+      <div className="card my-5">
+        <div>
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Good morning {user?.name}</h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+          <InfoCard
+          icon = {<IoMdCard />} />
+          label = "Total Tasks"
+          value = {addThousandsSeparator(
+            dashboardData?.charts?.taskDistribution?.All || 0
+          )}
+          color = "bg-primary"
+        </div>
+      </div>
     </DashboardLayout>
     
   )
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -2,3 +2,12 @@ export const validateEmail = (email) => {
     const regex = /^[^s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email)
 }
+
+export const addThousandsSeparator = (num) => {
+    if (num == null || isNaN(num)) return ""
+
+    const [intPart, fractionPart] = num.toString().split(".")
+    const formattedInteger = intPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
+    return fractionPart ? `${formattedInteger}.${fractionPart}` : formattedInteger
+}
